f74f7ca56412c2aa5410214e378152c631af7d84
Jovan Nikolic <jovannikolic@gmail.com>
2022-01-12 08:54:52 +0100

    Prvi zadatak

---
+++ src/d_10_01_2022/Proizvod.java
@@ -0,0 +1,16 @@
+package d_10_01_2022;
+
+public class Proizvod {
+
+	String naziv;
+	double cena;
+	double tezina;
+	public void stampaj() {
+		System.out.println("Naziv  " + this.naziv);
+		System.out.println("Cena  " + this.cena);
+		System.out.println("Tezina  " + this.tezina);
+		System.out.println();
+		
+	}
+	
+}
---
+++ src/d_10_01_2022/zadatak1.java
@@ -0,0 +1,20 @@
+package d_10_01_2022;
+
+public class zadatak1 {
+
+	public static void main(String[] args) {
+		Proizvod prvi= new Proizvod();
+		prvi.naziv ="Brasno";
+		prvi.cena=56.99;
+		prvi.tezina=500;
+		prvi.stampaj();
+		Proizvod drugi= new Proizvod();
+		drugi.naziv ="Secer";
+		drugi.cena=99.99;
+		drugi.tezina=500;
+		drugi.stampaj();
+
+
+	}
+
+}
